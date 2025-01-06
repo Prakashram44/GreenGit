52886a774197fcb43f8c7b601a9c451c278e9e25
Prakashram44 <divyaprakash4493@gmail.com>
2025-01-06 18:49:36 +0530

    Bro First Come First Serve

---
+++ src/test/java/com/omrbranch/GreenGit.java
@@ -0,0 +1,17 @@
+package com.omrbranch;
+
+public class GreenGit {
+	
+	public void deposit() {
+		
+		System.out.println("Deposit");
+	
+	}
+	
+	public void savings() {
+		
+		System.out.println("Savings");
+	
+	}
+
+}
